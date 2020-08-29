e638b91d23fdafffe60d3226da7cac2b6c694bbb
CrystallDEV <m.hasselder@gmx.de>
2020-08-29 20:33:23 +0200

    Project Creation

---
+++ src/main/java/dev/crystall/npclib/Npclib.java
@@ -0,0 +1,17 @@
+package dev.crystall.npclib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Npclib extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    // Plugin startup logic
+
+  }
+
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
+}
